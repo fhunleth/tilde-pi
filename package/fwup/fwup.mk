7b3f795eec4c80b75253567d96d0109a50291eaf
Frank Hunleth <fhunleth@troodon-software.com>
2015-05-16 14:17:52 -0400

    fwup: bump to unreleased version
    This will help me verify some new features.

--- package/fwup/fwup.mk
+++ package/fwup/fwup.mk
@@ -4,12 +4,12 @@
 #
 #############################################################
 
-FWUP_VERSION = v0.1.1
+FWUP_VERSION = 34cbe0a06c3fe5ca05be0b9b83995f66b5c9530d
 FWUP_SITE = $(call github,fhunleth,fwup,$(FWUP_VERSION))
 FWUP_LICENSE = Apache-2.0
 FWUP_LICENSE_FILES = COPYING
 FWUP_AUTORECONF = YES
-FWUP_DEPENDENCIES = libconfuse libarchive
+FWUP_DEPENDENCIES = libconfuse libarchive libsodium
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
